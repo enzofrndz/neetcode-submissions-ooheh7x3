610086768560c0da0f64269565cde6b79903a714
Enzo Fernandez <127005087+enzofrndz@users.noreply.github.com>
2026-09-04 20:13:37 -0400

    Add: is-anagram - submission-6

---
+++ Data Structures & Algorithms/is-anagram/submission-6.py
@@ -0,0 +1,20 @@
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        if len(s) != len(t):
+            return False
+        
+        hash1, hash2 = {}, {}
+
+        for n in s:
+            if n in hash1:
+                hash1[n] = hash1[n] + 1
+            else:
+                hash1[n] = 1
+        for m in t:
+            if m in hash2:
+                hash2[m] = hash2[m] + 1
+            else:
+                hash2[m] = 1
+        
+        return hash1 == hash2
+        
